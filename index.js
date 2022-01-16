e138942d1355dd0dd1894c98cc04dad0ea933e04
39coleworld <vinicius.billabong_95@hotmail.com>
2022-01-16 09:41:06 -0300

    commit-01

---
+++ index.js
@@ -0,0 +1,11 @@
+function colorChange() {
+    document.getElementById("text").style.borderColor='#1877F2';
+    document.getElementById("text").style.boxShadow='0 0 10px 5px rgb(24 119 242 / 7%';
+}
+
+function colorChangeItemTwo() {
+    document.getElementById("password").style.borderColor='#1877F2';
+    document.getElementById("password").style.boxShadow='0 0 10px 5px rgb(24 119 242 / 7%';
+}
+
+console.log(colorChange)
